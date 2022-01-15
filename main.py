9c7c7f1acd64aa09c692aea1cb7a8254abbcce69
bohanli <bl243@duke.edu>
2022-01-15 01:27:54 +0000

    add more conversions

--- main.py
+++ main.py
@@ -11,7 +11,41 @@ async def root():
 async def transferLbtoKg(num1: int):
     kg = format(0.46 * num1, ".2f")
     # kg = num1*2
-    return {"Kg":kg}
+    return {"kg":kg}
+
+async def transferOztoG(num1: int):
+    g = format(28.35 * num1, ".2f")
+    # kg = num1*2
+    return {"g":g}
+    
+async def transferGallontoL(num1: int):
+    l = format(3.79 * num1, ".2f")
+    # kg = num1*2
+    return {"l":l}
+    
+@app.get("/miletokm/{num1}")
+async def transferMiletoKm(num1: int):
+    km = format(1.6 * num1, ".2f")
+    # kg = num1*2
+    return {"km":km}
+    
+@app.get("/yardtom/{num1}")
+async def transferYardtoM(num1: int):
+    m = format(0.91 * num1, ".2f")
+    # kg = num1*2
+    return {"m":m}
+
+@app.get("/feettocm/{num1}")
+async def transferFeettoCm(num1: int):
+    cm = format(30.48 * num1, ".2f")
+    # kg = num1*2
+    return {"cm":cm}
+    
+@app.get("/inchtocm/{num1}")
+async def transferInchtoCm(num1: int):
+    cm = format(2.54 * num1, ".2f")
+    # kg = num1*2
+    return {"cm":cm}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0') 
